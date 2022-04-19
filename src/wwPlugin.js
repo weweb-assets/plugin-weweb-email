871227c5d98a2d25efc2938a1d31dab7ed0f2539
Dams-d <damien.duro8@gmail.com>
2022-04-19 15:44:44 +0200

    Change actions parameters into an object

--- src/wwPlugin.js
+++ src/wwPlugin.js
@@ -6,7 +6,7 @@ export default {
     /*=============================================m_ÔÔ_m=============================================\
         WeWeb Email API
     \================================================================================================*/
-    async sendEmail(recipients, subject, content) {
+    async sendEmail({ recipients, subject, content }) {
         const designId = wwLib.wwWebsiteData.getInfo().id;
         await axios.post(`${wwLib.wwApiRequests._getApiUrl()}/designs/${designId}/send-email`, {
             recipients,
--- ww-config.js
+++ ww-config.js
@@ -1,14 +1,9 @@
 export default {
     editor: {},
-    functions: [
+    actions: [
         {
             name: 'Send an email',
             code: 'sendEmail',
-            parameters: [
-                { name: 'Recipients', type: 'array' },
-                { name: 'Subject', type: 'string' },
-                { name: 'Content', type: 'string' },
-            ],
             isAsync: true,
             /* wwEditor:start */
             edit: () => import('./src/components/SendEmail.vue'),
