d22ab642b80683f5bd416df4763d85f9c00c0f4a
Marc Fabre <marc@weweb.io>
2024-05-31 11:59:57 +0200

    Added copilot info

--- ww-config.js
+++ ww-config.js
@@ -7,6 +7,17 @@ export default {
             isAsync: true,
             /* wwEditor:start */
             edit: () => import('./src/components/SendEmail.vue'),
+            copilot: {
+                description: 'Send an email to multiple recipients',
+                returns: 'null',
+                schema: `{
+                    args: {
+                        content: __VALUE__ (Content of the email),
+                        subject: __VALUE__ (Subject of the email),
+                        recipients: __VALUE__ (Array of email addresses to send the email to)
+                    }
+                }`
+            }
             /* wwEditor:end */
         },
     ],
